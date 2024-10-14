d23382b02fc9d6cc0414c38492dd25fa27b71261
HakanUca <hakanhuca@gmail.com>
2024-10-14 16:58:01 +0300

    points and lines for the hand game objects

---
+++ Kiosk-GK-Project/Assets/HandTracking.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    public UDPReceive udpReceive;
+    public GameObject[] handPoints;
+    void Start()
+    {
+        
+    }
+
+    void Update()  
+    {
+        
+    }
+}
