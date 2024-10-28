20f6e9137fdd703ebc94af4d83c0eea627700a34
HakanUca <hakanhuca@gmail.com>
2024-10-28 15:01:05 +0300

    goal line is completed

---
+++ Kiosk-GK-Project/Assets/Scripts/GoalCounter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalCounter : MonoBehaviour
+{
+    public Text goalText;  // Reference to UI Text to display the goal count
+    private int goalCount = 0;
+
+    // This method is called when another collider enters the trigger collider
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")  // Check if the colliding object is the Ball
+        {
+            goalCount++;  // Increase the goal count
+            UpdateGoalText();  // Update the displayed text
+        }
+    }
+
+    // Update the goal count text in the UI
+    private void UpdateGoalText()
+    {
+        goalText.text = "Goals: " + goalCount;
+    }
+}
