622a262a11b8a726569d0ba4c7316d04ddd0f8d2
HakanUca <hakanhuca@gmail.com>
2024-10-30 09:21:49 +0300

    new game over logic

--- Kiosk-GK-Project/Assets/Scripts/GoalCounter.cs
+++ Kiosk-GK-Project/Assets/Scripts/GoalCounter.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GoalCounter : MonoBehaviour
 {
     public Text goalText;  // Reference to UI Text to display the goal count
     private int goalCount = 0;
-
+    public GameObject gameOverText;
+    public GameObject ballSpawner;
+    
+    
     // This method is called when another collider enters the trigger collider
     private void OnTriggerEnter(Collider other)
     {
@@ -20,5 +26,25 @@ public class GoalCounter : MonoBehaviour
     private void UpdateGoalText()
     {
         goalText.text = "GOALS: " + goalCount;
+        if (goalCount == 10)
+        {
+            CallGameOverFunction();
+
+        }
+    }
+
+    // The function close the game after the reached maximum goal
+    private void CallGameOverFunction()
+    {
+        ballSpawner.SetActive(false);
+        gameOverText.SetActive(true); // Sets the game over text
+        StartCoroutine(ReloadSceneAfterDelay(3f)); // Starts coroutine with 3 seconds delay
+    }
+
+    private IEnumerator ReloadSceneAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // Waits for specified delay
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name); // Reloads the current scene
     }
 }
